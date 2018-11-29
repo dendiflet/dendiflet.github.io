6be20e3f6b8cb68a03103843843aa669e4f2ac9f
dendiflet <bretin.iannis@gmail.com>
2018-11-29 18:12:49 +0100

    gréation des fichers

---
+++ fistjava.js
@@ -0,0 +1,22 @@
+
+
+var nom = prompt("c'est quoi ton blase?");
+
+
+if ( nom == null ) {
+	nom ="ROBERT", alert("je vais donc t'appeler ROBERT");
+}
+
+var question = ' ,veux tu te faire rick roller? cliquez sur "OK" si c\'est le cas.';
+var questionnom = nom + question
+
+if (confirm(questionnom)) {
+    alert("c'est parti !"); location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+}
+
+else {
+    alert("ok, pas grave (j'espère que tu bloque les fenetre pop-up)"); window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank');
+  win.focus();
+}
+
